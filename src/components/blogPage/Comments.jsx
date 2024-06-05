21bf31e102b5123df65055b187d61de7f2cd656c
Priya-Pareek01 <priyapareek_9098>
2024-06-05 23:53:23 +0530

    ..

--- src/components/blogPage/Comments.jsx
+++ src/components/blogPage/Comments.jsx
@@ -21,9 +21,8 @@ const Comments = ({postSlug}) =>{
     const {status} = useSession();
     const [desc, setDesc] = useState("");
     const [textareaValue, setTextareaValue] = useState("");
-    if(!BASE_API_URL) return null;
-
-    const {data, mutate, isLoading}  = useSWR(`${BASE_API_URL}/api/comments?postSlug=${postSlug}`,
+    
+    const {data, mutate, isLoading}  = useSWR(`/api/comments?postSlug=${postSlug}`,
         fetcher
     )
    
