64d4c553d2a10896382b1daa0566a613a3cf5a7e
Priya-Pareek01 <priyapareek_9098>
2024-06-11 15:20:19 +0530

    adjusted some margin padding.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -9,7 +9,7 @@ const Blog =  ({searchParams})=>{
     
     return(
         <div className="w-screen"> 
-            <h1 className=" bg-green-500 md:my-8 mt-6 mx-auto text-center py-2 w-1/2 text-white text-xl capitalize">
+            <h1 className="bg-green-500 md:my-6 mt-10 mx-auto text-center py-2 w-1/2 text-white text-xl capitalize">
                 {cat} Blogs </h1>
             <div className="flex w-screen my-14  gap-12">
                 <CardList page={page} cat= {cat}/>
