61f5f7cb9533aa61104097f3a5071ee28feabaa2
Priya-Pareek01 <priyapareek_9098>
2024-06-21 00:09:20 +0530

    error fixing.

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,201 +1,4 @@
-// "use client"
-// import { useEffect, useState } from "react";
-// import "react-quill/dist/quill.bubble.css";
-// import dynamic from "next/dynamic";
-// import { IoIosAddCircleOutline } from "react-icons/io";
-// import { FiUpload } from "react-icons/fi";
-// import { FaImages } from "react-icons/fa";
-// import { MdVideoLibrary } from "react-icons/md";
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-// import { app } from "@/utils/firebase";
-// import Modals from "@/components/Modals";
-// import { BASE_API_URL } from "@/utils/constants";
-
-// import { getAuth, signInWithCustomToken } from 'firebase/auth';
-
-
-// const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-
-// const Write = () =>{
-//     const {status} = useSession();
-//     const router = useRouter();
-//     const [open, setOpen] = useState(false);
-
-//     const [value, setValue] = useState("");
-//     const [titleValue, setTitleValue] = useState("");
-//     const [file , setFile] = useState("");
-//     const [media, setMedia] = useState("");
-//     const storage = getStorage(app);
-//     const [catValue, SetCatValue] = useState("");
-//     const [categories, setCategories] = useState(null);
-
-//     const [isDialogboxOpen, setIsDialogboxOpen] = useState(false);
-//     const [dialogBoxValue, setDialogBoxValue] = useState(null);
-//     const [imgUploadTxt, setImgUploadTxt] = useState(null);
-
-//     const GetCategoryItems = async() =>{
-//         const res = await fetch(`${BASE_API_URL}/api/categories`, 
-//             {cache:"no-store"});
-//         if(!res.ok){
-//             console.error("failed");
-//         }
-//         return res.json();
-//     }
-
-//     useEffect(() => {
-//         const Get = async() =>{
-//             const cat = await GetCategoryItems();
-//             setCategories(cat);
-//         }
-//         Get();
-//     }, []);
-
-        
-//     useEffect(() =>{
-//         const Upload = () => { 
-//             const name = new Date().getTime() + file.name;
-//             const storageRef = ref(storage, name);
-        
-//             const uploadTask = uploadBytesResumable(storageRef, file);
-        
-//             uploadTask.on('state_changed', 
-//             (snapshot) => {
-//                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//                 switch (snapshot.state) {
-//                     case 'paused':
-//                         console.log('Upload is paused');
-//                      break;
-//                     case 'running':
-//                         console.log('Upload is running');
-//                     break;
-//                 }
-//             }, 
-//             (error) => {
-//                 console.error('Upload error:', error);
-//             }, 
-//                 () => {
-//                     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-//                         setImgUploadTxt("Image uploaded successfully!");
-//                         setIsDialogboxOpen(true);
-//                         setMedia(downloadURL);
-//                 });
-//             }
-//             );
-//         };
-
-//         if (file) Upload();
-
-//     }, [file, storage])
-
-//     const handleSubmit= async() =>{
-//         const res = await fetch("/api/posts", {
-//             method: "POST",
-//             body: JSON.stringify({
-//                 desc: value,
-//                 img: media,
-//                 title: titleValue,
-//                 catSlug: catValue.trim(),
-//                 slug: `${titleValue.toLowerCase().replace(/\s+/g, '-')}-${Date.now()}`, 
-//             })
-//         });
-
-//         setIsDialogboxOpen(true);
-        
-//         if(res.ok){
-//             setDialogBoxValue("Successfully Published!");
-//             router.refresh();
-//             router.push("/")
-//         }else{
-//             setDialogBoxValue("Sorry! Something went wrong.. ");
-//         }
-//     }
-
-
-//     if(status === "loading"){
-//         return(
-//             <div> LOADING... </div>
-//         );
-//     }
-
-//     if(status === "unauthenticated"){
-//         router.push("/login");
-//     }
-//     return(
-//         <div className="flex flex-col  pt-10 md:ml-[220px] relative  ">
-
-//             <select className="outline-none text-[#9c9c99] ml-4 md:p-2 p-1 w-fit rounded-md bg-[#da85c731] capitalize"
-//                     onChange={(e) => SetCatValue(e.target.value)}>
-//                     <option  defaultValue={"-Select category-"} disabled selected hidden>
-//                         -Select category-
-//                     </option>
-//                 {categories && categories.map((item) => (
-//                     <option key={item?.id} value={item?.slug} > {item.slug} </option>
-//                 ))}
-//             </select>
-
-//             <input type="text"  placeholder="Title" onChange={(e) =>{
-//                     e.preventDefault;
-//                     setTitleValue(e.target.value);
-//                     }} value={titleValue}
-//                 className="p-[25px] md:text-4xl text-2xl bg-transparent 
-//                  outline-none"/> 
-
-//             <div className="writePageItems">
-//                 <button onClick={() => setOpen(!open)}>
-//                     <IoIosAddCircleOutline className="h-8 w-8 text-[#b3b3b1]"/>
-//                 </button>
-
-//                 {open && 
-//                 <div className="flex gap-2 ">
-//                     <input type="file" id="image" 
-//                         onChange={(e) => setFile(e.target.files[0])} 
-//                         className="hidden" />
-
-//                     <button className="border rounded-[50%] p-[6px]  border-[#1a8917]"> 
-//                         <label htmlFor="image">
-//                             <FaImages className="h-4 w-4 text-[#b3b3b1]"/>
-//                         </label>
-//                     </button>
-
-//                     <button className="border rounded-[50%] p-[6px] border-[#1a8917]"> 
-//                         <FiUpload className="h-4 w-4 text-[#b3b3b1]"/>
-//                     </button>
-
-//                     <button className="border rounded-[50%] p-[6px] border-[#1a8917]"> 
-//                         <MdVideoLibrary className="h-4 w-4 text-[#b3b3b1]"/>
-//                     </button>
-//                 </div>}
-
-//             </div>
-//             <div className="pl-10 md:ml-0">
-//                 <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story.."/>
-//             </div>
-            
-//                 <button className="bg-[#1a8917] rounded-lg p-2 text-xs text-white absolute top-12 right-3 "
-//                     onClick={handleSubmit}> 
-//                     Publish 
-//                 </button>
-
-//                 <div className="md:inline-block hidden">
-//                 <Modals isDialogboxOpen= {isDialogboxOpen} onRequestClose={() => (setIsDialogboxOpen(false),
-//                                                                                 setDialogBoxValue(" "))} 
-//                                                                                 showCloseBtn={true}>
-//                     <h1 className="py-1 text-green-800 font-semibold text-base">
-//                         {dialogBoxValue? dialogBoxValue : imgUploadTxt }
-//                     </h1>
-//                 </Modals>
-//                 </div>
-
-//         </div>
-//     );
-// }
-
-// export default Write;
-
-
-"use client";
+"use client"
 import { useEffect, useState } from "react";
 import "react-quill/dist/quill.bubble.css";
 import dynamic from "next/dynamic";
@@ -206,91 +9,84 @@ import { MdVideoLibrary } from "react-icons/md";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { auth, storage } from "@/utils/firebase";
+import { app } from "@/utils/firebase";
 import Modals from "@/components/Modals";
 import { BASE_API_URL } from "@/utils/constants";
-import { signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
-const Write = () => {
-    const { data: session, status } = useSession();
+const Write = () =>{
+    const {status} = useSession();
     const router = useRouter();
     const [open, setOpen] = useState(false);
 
     const [value, setValue] = useState("");
     const [titleValue, setTitleValue] = useState("");
-    const [file, setFile] = useState(null);
+    const [file , setFile] = useState("");
     const [media, setMedia] = useState("");
-    const [catValue, setCatValue] = useState("");
+    const storage = getStorage(app);
+    const [catValue, SetCatValue] = useState("");
     const [categories, setCategories] = useState(null);
 
     const [isDialogboxOpen, setIsDialogboxOpen] = useState(false);
     const [dialogBoxValue, setDialogBoxValue] = useState(null);
     const [imgUploadTxt, setImgUploadTxt] = useState(null);
 
-    const GetCategoryItems = async () => {
-        const res = await fetch(`${BASE_API_URL}/api/categories`, { cache: "no-store" });
-        if (!res.ok) {
+    const GetCategoryItems = async() =>{
+        const res = await fetch(`${BASE_API_URL}/api/categories`, 
+            {cache:"no-store"});
+        if(!res.ok){
             console.error("failed");
         }
         return res.json();
-    };
+    }
 
     useEffect(() => {
-        const Get = async () => {
+        const Get = async() =>{
             const cat = await GetCategoryItems();
             setCategories(cat);
         }
         Get();
     }, []);
 
-    const handleAuthAndUpload = async () => {
-        if (!file) return;
-
-        let provider;
-
-        // Determine provider based on session
-        if (session.provider === 'google') {
-            provider = new GoogleAuthProvider();
-        } else if (session.provider === 'github') {
-            provider = new GithubAuthProvider();
-        }
-
-        try {
-            await signInWithPopup(auth, provider);
+        
+    useEffect(() =>{
+        const Upload = () => { 
             const name = new Date().getTime() + file.name;
             const storageRef = ref(storage, name);
+        
             const uploadTask = uploadBytesResumable(storageRef, file);
-
+        
             uploadTask.on('state_changed', 
-                (snapshot) => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log(`Upload is ${progress}% done`);
-                }, 
-                (error) => {
-                    console.error('Upload error:', error);
-                }, 
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                     break;
+                    case 'running':
+                        console.log('Upload is running');
+                    break;
+                }
+            }, 
+            (error) => {
+                console.error('Upload error:', error);
+            }, 
                 () => {
                     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                         setImgUploadTxt("Image uploaded successfully!");
                         setIsDialogboxOpen(true);
                         setMedia(downloadURL);
-                    });
-                }
+                });
+            }
             );
-        } catch (error) {
-            console.error('Authentication error:', error);
-        }
-    };
+        };
 
-    useEffect(() => {
-        if (file) {
-            handleAuthAndUpload();
-        }
-    }, [file]);
+        if (file) Upload();
 
-    const handleSubmit = async () => {
+    }, [file, storage])
+
+    const handleSubmit= async() =>{
         const res = await fetch("/api/posts", {
             method: "POST",
             body: JSON.stringify({
@@ -303,90 +99,91 @@ const Write = () => {
         });
 
         setIsDialogboxOpen(true);
-
-        if (res.ok) {
+        
+        if(res.ok){
             setDialogBoxValue("Successfully Published!");
             router.refresh();
-            router.push("/");
-        } else {
+            router.push("/")
+        }else{
             setDialogBoxValue("Sorry! Something went wrong.. ");
         }
-    };
+    }
 
-    if (status === "loading") {
-        return (
+
+    if(status === "loading"){
+        return(
             <div> LOADING... </div>
         );
     }
 
-    if (status === "unauthenticated") {
+    if(status === "unauthenticated"){
         router.push("/login");
     }
+    return(
+        <div className="flex flex-col  pt-10 md:ml-[220px] relative  ">
 
-    return (
-        <div className="flex flex-col pt-10 md:ml-[220px] relative">
             <select className="outline-none text-[#9c9c99] ml-4 md:p-2 p-1 w-fit rounded-md bg-[#da85c731] capitalize"
-                onChange={(e) => setCatValue(e.target.value)}>
-                <option defaultValue={"-Select category-"} disabled selected hidden>
-                    -Select category-
-                </option>
+                    onChange={(e) => SetCatValue(e.target.value)}>
+                    <option  defaultValue={"-Select category-"} disabled selected hidden>
+                        -Select category-
+                    </option>
                 {categories && categories.map((item) => (
-                    <option key={item?.id} value={item?.slug}> {item.slug} </option>
+                    <option key={item?.id} value={item?.slug} > {item.slug} </option>
                 ))}
             </select>
 
-            <input type="text" placeholder="Title" onChange={(e) => {
-                e.preventDefault;
-                setTitleValue(e.target.value);
-            }} value={titleValue}
+            <input type="text"  placeholder="Title" onChange={(e) =>{
+                    e.preventDefault;
+                    setTitleValue(e.target.value);
+                    }} value={titleValue}
                 className="p-[25px] md:text-4xl text-2xl bg-transparent 
-                 outline-none" />
+                 outline-none"/> 
 
             <div className="writePageItems">
                 <button onClick={() => setOpen(!open)}>
-                    <IoIosAddCircleOutline className="h-8 w-8 text-[#b3b3b1]" />
+                    <IoIosAddCircleOutline className="h-8 w-8 text-[#b3b3b1]"/>
                 </button>
 
-                {open &&
-                    <div className="flex gap-2 ">
-                        <input type="file" id="image"
-                            onChange={(e) => setFile(e.target.files[0])}
-                            className="hidden" />
+                {open && 
+                <div className="flex gap-2 ">
+                    <input type="file" id="image" 
+                        onChange={(e) => setFile(e.target.files[0])} 
+                        className="hidden" />
 
-                        <button className="border rounded-[50%] p-[6px]  border-[#1a8917]">
-                            <label htmlFor="image">
-                                <FaImages className="h-4 w-4 text-[#b3b3b1]" />
-                            </label>
-                        </button>
+                    <button className="border rounded-[50%] p-[6px]  border-[#1a8917]"> 
+                        <label htmlFor="image">
+                            <FaImages className="h-4 w-4 text-[#b3b3b1]"/>
+                        </label>
+                    </button>
 
-                        <button className="border rounded-[50%] p-[6px] border-[#1a8917]">
-                            <FiUpload className="h-4 w-4 text-[#b3b3b1]" />
-                        </button>
+                    <button className="border rounded-[50%] p-[6px] border-[#1a8917]"> 
+                        <FiUpload className="h-4 w-4 text-[#b3b3b1]"/>
+                    </button>
 
-                        <button className="border rounded-[50%] p-[6px] border-[#1a8917]">
-                            <MdVideoLibrary className="h-4 w-4 text-[#b3b3b1]" />
-                        </button>
-                    </div>}
+                    <button className="border rounded-[50%] p-[6px] border-[#1a8917]"> 
+                        <MdVideoLibrary className="h-4 w-4 text-[#b3b3b1]"/>
+                    </button>
+                </div>}
 
             </div>
             <div className="pl-10 md:ml-0">
-                <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story.." />
+                <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story.."/>
             </div>
+            
+                <button className="bg-[#1a8917] rounded-lg p-2 text-xs text-white absolute top-12 right-3 "
+                    onClick={handleSubmit}> 
+                    Publish 
+                </button>
 
-            <button className="bg-[#1a8917] rounded-lg p-2 text-xs text-white absolute top-12 right-3"
-                onClick={handleSubmit}>
-                Publish
-            </button>
-
-            <div className="md:inline-block hidden">
-                <Modals isDialogboxOpen={isDialogboxOpen} onRequestClose={() => (setIsDialogboxOpen(false),
-                    setDialogBoxValue(" "))} 
-                    showCloseBtn={true}>
+                <div className="md:inline-block hidden">
+                <Modals isDialogboxOpen= {isDialogboxOpen} onRequestClose={() => (setIsDialogboxOpen(false),
+                                                                                setDialogBoxValue(" "))} 
+                                                                                showCloseBtn={true}>
                     <h1 className="py-1 text-green-800 font-semibold text-base">
-                        {dialogBoxValue ? dialogBoxValue : imgUploadTxt}
+                        {dialogBoxValue? dialogBoxValue : imgUploadTxt }
                     </h1>
                 </Modals>
-            </div>
+                </div>
 
         </div>
     );
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,4 @@
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAhNtPQHw12pf_w1Yrfh-8lzyIyw_uTPoA",
-//   authDomain: "musings-cd129.firebaseapp.com",
-//   projectId: "musings-cd129",
-//   storageBucket: "musings-cd129.appspot.com",
-//   messagingSenderId: "370218263209",
-//   appId: "1:370218263209:web:cb752cb8214e3191e3ffb8"
-// };
-
-// export const app = initializeApp(firebaseConfig);
-
-
-// firebase.js
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
@@ -26,10 +9,4 @@ const firebaseConfig = {
   appId: "1:370218263209:web:cb752cb8214e3191e3ffb8"
 };
 
-// Initialize Firebase app
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-export { auth, storage, app };
+export const app = initializeApp(firebaseConfig);
